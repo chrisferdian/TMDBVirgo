872f33508bb9cea30b1cc0e167121975a7f1b657
Chris Ferdian <chris.ferdian@gmail.com>
2022-05-16 08:12:33 +0700

    Initial commit

---
+++ TMDBVirgo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMDBVirgo
+//
+//  Created by Indo Teknologi Utama on 16/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
